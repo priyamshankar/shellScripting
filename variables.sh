a9e57312f92247397a770ed13a3dddbad8cb7057
priyam <priyamshankar.5@gmail.com>
2022-05-11 22:39:52 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,15 @@
+#the variables are of two types system variables and user defined variable
+#mostly defined in upper cases the system variables
+
+echo $BASH
+# bash is a system variable, before a variable a $symbol is used
+echo $BASH_VERSION
+
+
+#user defined variable
+name=priyamshankar
+echo $name
+
+echo "the name is " $name
+
+#the variable name should not start with no.
